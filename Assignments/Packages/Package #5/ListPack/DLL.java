1cac4a3952f581bfe533e37616b61eb37b2b1e77
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-02-02 15:51:58 +0530

    Example of creating a package class of creating Doubly Linked List

--- Assignments/Packages/Package #5/ListPack/DLL.java
+++ Assignments/Packages/Package #5/ListPack/DLL.java
@@ -1,5 +1,3 @@
-import java.io.*;
-
 public class DLL
 {
 	public void doubly(int n,String node[])
@@ -7,11 +5,11 @@ public class DLL
 		System.out.print("Displaying Doubly Linked List: ");
 		for(int i=0;i<n;i++)
 		{
-			if(i==0)
+			if(i == 0)
 			{
 				System.out.print("X<-");
 			}
-			if(i==n-1)
+			if(i == n-1)
 			{
 				System.out.print(node[i]+"->X");
 			}
