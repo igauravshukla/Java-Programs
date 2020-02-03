dc95bba38926fd12c5f0a3666017119dedd89a22
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-02-03 16:03:47 +0530

    Example of Vehicle inheritance

--- Assignments/Inheritance and Interfaces/Main1.java
+++ Assignments/Inheritance and Interfaces/Main1.java
@@ -10,41 +10,49 @@ class Vehicle
 {
 	String company;
 	double price;
+
 	Vehicle(String c,double p)
 	{
-		this.company=c;
-		this.price=p;
+		this.company = c;
+		this.price = p;
 	}
+
 	void display1()
 	{
-		System.out.println("Company name = "+company);
+		System.out.println("\nCompany name = "+company);
 		System.out.println("Vehicle price = "+price);		
 	}
 }
+
 class Transport extends Vehicle
 {
 	int val_no,period;
+
 	Transport(String c,double p,int vn,int per)
 	{
 		super(c,p);
-		val_no=vn;
-		period=per;
+		val_no = vn;
+		period = per;
 	}
+
 	void d2()
 	{
 		System.out.println("Validity No. = "+val_no);
 		System.out.println("Period = "+period);
 	}
 }
+
 class Private extends Vehicle
 {
 	String oname,oaddr;
+
 	Private(String c,double p,String name,String addr)
 	{
 		super(c,p);
-		oname=name;
-		oaddr=addr;
+		oname = name;
+		oaddr = addr;
 	}
+
 	void d3()
 	{
 		System.out.println("Owner Name = "+oname);
@@ -60,50 +68,52 @@ class Main1
 		String comp,oname,oaddr;
 		double price;
 		int vno,period;
+
 		do
 		{
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			System.out.println("\n1.Transport Vehicle\n2.Private Vehicle\n3.Exit\n");
-			System.out.print("Enter the type of vehicle: ");
-			ch=Integer.parseInt(br.readLine());
+			System.out.print("Enter the type of vehicle : ");
+			ch = Integer.parseInt(br.readLine());
 			switch(ch)
 			{
-				case 1:	System.out.print("Number of vehicles: ");
-						n=Integer.parseInt(br.readLine());
-						Transport T[]=new Transport[n];
+				case 1:	System.out.print("\nNumber of vehicles : ");
+						n = Integer.parseInt(br.readLine());
+						Transport T[] = new Transport[n];
 						for(int i=0;i<n;i++)
 						{
-							System.out.print("Enter the company name : ");
-							comp=br.readLine();
-							System.out.print("Enter the Price: ");
-							price=Double.parseDouble(br.readLine());
-							System.out.print("Enter vaidity number: ");
-							vno=Integer.parseInt(br.readLine());
-							System.out.print("Enter period: ");
-							period=Integer.parseInt(br.readLine());
-							T[i]=new Transport(comp,price,vno,period);						
+							System.out.print("\nEnter the company name : ");
+							comp = br.readLine();
+							System.out.print("Enter the Price : ");
+							price = Double.parseDouble(br.readLine());
+							System.out.print("Enter vaidity number : ");
+							vno = Integer.parseInt(br.readLine());
+							System.out.print("Enter period : ");
+							period = Integer.parseInt(br.readLine());
+							T[i] = new Transport(comp,price,vno,period);						
 						}
-						System.out.println("Details are \n");
+						System.out.println("\nDetails are \n");
 						for(int i=0;i<n;i++)
 						{
 							T[i].display1();
 							T[i].d2();
 						}
 						break;
-				 case 2:	System.out.print("Number of vehicles: ");
-						n=Integer.parseInt(br.readLine());
-						Private P[]=new Private[n];
+
+				 case 2:	System.out.print("\nNumber of vehicles : ");
+						n = Integer.parseInt(br.readLine());
+						Private P[] = new Private[n];
 						for(int i=0;i<n;i++)
 						{
-							System.out.print("Enter the company name : ");
-							comp=br.readLine();
-							System.out.print("Enter the Price: ");
-							price=Double.parseDouble(br.readLine());
-							System.out.print("Enter owner name: ");
-							oname=br.readLine();
-							System.out.print("Enter owner address: ");
-							oaddr=br.readLine();
-							P[i]=new Private(comp,price,oname,oaddr);						
+							System.out.print("\nEnter the company name : ");
+							comp = br.readLine();
+							System.out.print("Enter the Price : ");
+							price = Double.parseDouble(br.readLine());
+							System.out.print("Enter owner name : ");
+							oname = br.readLine();
+							System.out.print("Enter owner address : ");
+							oaddr = br.readLine();
+							P[i] = new Private(comp,price,oname,oaddr);						
 						}
 						System.out.print("\nDetails are:\n");
 						for(int i=0;i<n;i++)
@@ -112,32 +122,37 @@ class Main1
 							P[i].d3();
 						}
 						break;
-				case 3:System.exit(0);
+
+				case 3:	System.exit(0);
 				}
 		}while(ch!=3);
 	}
 }
 
-/*output
+/*
 
 $ javac Main1.java
+
 $ java Main1
 
 1.Transport Vehicle
 2.Private Vehicle
 3.Exit
 
-Enter the type of vehicle: 1
-Number of vehicles: 2   
+Enter the type of vehicle : 1
+
+Number of vehicles : 2   
+
 Enter the company name : Truck No. 1
-Enter the Price: 23000
-Enter vaidity number: 09876
-Enter period: 3
+Enter the Price : 23000
+Enter vaidity number : 09876
+Enter period : 3
 Enter the company name : Truck No. 2
 Enter the Price: 12000
 Enter vaidity number: 876544
 Enter period: 2
-Details are 
+
+Details are :
 
 Company name = Truck No. 1
 Vehicle price = 23000.0
@@ -153,7 +168,9 @@ Period = 2
 3.Exit
 
 Enter the type of vehicle: 2
+
 Number of vehicles: 2
+
 Enter the company name : Maruti
 Enter the Price: 34000
 Enter owner name: Gaurav Shukla
@@ -163,7 +180,8 @@ Enter the Price: 89000
 Enter owner name: Bhai Don Patil    
 Enter owner address: Talegaon Rohi, Nashik
 
-Details are:
+Details are :
+
 Company name = Maruti
 Vehicle price = 34000.0
 Owner Name = Gaurav Shukla
