0748126c11b31093e7704755e961cb7e89fa28b6
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-02-04 16:25:42 +0530

    Example of Student object cloning (multiple clones)

--- Assignments/Inheritance and Interfaces/Main6.java
+++ Assignments/Inheritance and Interfaces/Main6.java
@@ -1,29 +1,39 @@
+/*
+
+Define a class “Student” which has members Roll_no, name. Define another class “CR” which has members class name and extends Student. Create an object of the CR class and make multiple clone of it. (Use the Cloneable interface). 
+
+*/
+
 import java.io.*;
 
 class Student
 {
 	int rno;
 	String name;
+
 	Student(int r,String nm)
 	{
-		rno=r;
-		name=nm;
+		rno = r;
+		name = nm;
 	}
 }
 
 class CR extends Student implements Cloneable
 {
 	String cname;
+
 	CR(int r,String nm,String c)
 	{
 		super(r,nm);
-		cname=c;
+		cname = c;
 	}
+
 	public CR clone() throws CloneNotSupportedException
 	{
 		CR C = (CR)super.clone();
 		return C;
 	}
+
 	public String toString()
 	{
 		return "Roll no = "+rno+" Name = "+name+" Class = "+cname;
@@ -34,16 +44,16 @@ class Main6
 {
 	public static void main(String args[]) throws IOException, CloneNotSupportedException
 	{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("\nEnter the number of clone objects to be made: ");
 		int n = Integer.parseInt(br.readLine());
-		System.out.print("\nEnter roll no of student:- ");
+		System.out.print("\nEnter roll no of student : ");
 		int rno = Integer.parseInt(br.readLine());
-		System.out.print("Enter name of student:- ");
+		System.out.print("Enter name of student :  ");
 		String nm = br.readLine();
-		System.out.print("Enter class of student:- ");
+		System.out.print("Enter class of student :  ");
 		String cls = br.readLine();
-		CR c= new CR(rno,nm,cls);
+		CR c = new CR(rno,nm,cls);
 		CR cl[] = new CR[n];
 		for(int i=0;i<n;i++)
 		{
@@ -53,16 +63,16 @@ class Main6
 	}
 }
 
-/*output
+/*
 
 $ javac Main6.java
 $ java Main6
 
 Enter the number of clone objects to be made: 3
 
-Enter roll no of student:- 1
-Enter name of student:- Gaurav
-Enter class of student:- MCA
+Enter roll no of student : 1
+Enter name of student : Gaurav
+Enter class of student : MCA
 Roll no = 1 Name = Gaurav Class = MCA
 Roll no = 1 Name = Gaurav Class = MCA
 Roll no = 1 Name = Gaurav Class = MCA
