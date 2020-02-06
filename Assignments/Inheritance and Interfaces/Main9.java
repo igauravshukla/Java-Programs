be4d8e6dcf59155e5dca826ec50fb4d6b7194440
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-02-06 16:41:27 +0530

    Example of abstract class Shape inheritance

--- Assignments/Inheritance and Interfaces/Main9.java
+++ Assignments/Inheritance and Interfaces/Main9.java
@@ -7,73 +7,79 @@ Create an abstract class Shape with methods calc_area and calc_volume. Derive th
 abstract class Shape
 {
 	double r,h;
+
 	Shape(double r,double h)
 	{
-		this.r=r;
-		this.h=h;
+		this.r = r;
+		this.h = h;
 	}
-	void calc_area()
-	{
-		System.out.println("Area : ");
-	}
-	void calc_vol()
-	{
-		System.out.println("volume : ");
-	}
+
+	abstract void calc_area();
+
+	abstract void calc_vol();
 }
 
 class Sphere extends Shape
 {
 	double A1,V1;
+
 	Sphere(double r)
 	{
 		super(r,0);
 	}
+
 	void calc_area()
-	{       
-		A1=(4*3.314*r*r);
-		System.out.println("\nArea of sphere:"+A1);
+	{
+		A1 = (4*3.314*r*r);
+		System.out.println("\nArea of sphere : "+A1);
 	}
+
 	void calc_vol()
 	{
-		V1=(1.334*3.14*r*r*r);
-		System.out.println("volume of sphere:" +V1);
+		V1 = (1.334*3.14*r*r*r);
+		System.out.println("volume of sphere : " +V1);
 	}
 }
 
 class Cone extends Shape
 {
 	double A2,V2;
+
 	Cone(double r,double h)
 	{
 		super(r,h);
 	}
+
 	void calc_area()
-	{       
-		A2=(3.14*r*Math.sqrt((r*r)+(h*h)));
+	{
+		A2 = (3.14*r*Math.sqrt((r*r)+(h*h)));
 		System.out.println("Area of Cone : "+A2);
 	}
+
 	void calc_vol()
 	{
-		V2=(0.334*3.14*r*r*h);
+		V2 = (0.334*3.14*r*r*h);
 		System.out.println("Volume of Cone : " +V2);
 	}
 }	
 class Cylinder extends Shape
 {
 	double A3,V3;
+
 	Cylinder(double r,double h)
 	{
 		super(r,h);
 	}
+
 	void calc_area()
-	{       
-		A3=(3-(3.14*r*r))+(h*(2-(3.14*r)));
+	{
+		A3 = (3-(3.14*r*r))+(h*(2-(3.14*r)));
 		System.out.println("Area of cylinder : "+A3);
 	}
+
 	void calc_vol()
 	{
-		V3=(3.14*r*r*h);
+		V3 = (3.14*r*r*h);
 		System.out.println("Volume of cylinder : " +V3);
 	}
 }
@@ -81,23 +87,27 @@ class Cylinder extends Shape
 class Box extends Shape
 {
 	double A4,V4,l,b;
+
 	void getb(double l1,double b1)
 	{
 		l=l1;
 		b=b1;
 	}
+
 	Box(double h)
 	{
 		super(0,h);
 	}
+
 	void calc_area()
-	{       
-		A4=2*((l*b)+(l*h)+(b*h));
+	{
+		A4 = 2*((l*b)+(l*h)+(b*h));
 		System.out.println("Area of box : "+A4);
 	}
+
 	void calc_vol()
 	{
-		V4=(l*b*h);
+		V4 = (l*b*h);
 		System.out.println("Volume of box : "+V4);
 	}
 }
@@ -109,12 +119,15 @@ class Main9
 		Sphere sp = new Sphere(5);
 		sp.calc_area();
 		sp.calc_vol();
-		Cone cn=new Cone(5,10);
+
+		Cone cn = new Cone(5,10);
 		cn.calc_area();
 		cn.calc_vol();
+
 		Cylinder cy = new Cylinder(5,10);
 		cy.calc_area();
 		cy.calc_vol();
+
 		Box b1 = new Box(10);
 		b1.getb(5,5);
 		b1.calc_area();
@@ -122,19 +135,19 @@ class Main9
 	}
 }
 
-/*output
+/*
 
-$ vi shapemain.java 
-$ javac shapemain.java
-$ java shapemain
+$ javac Main9.java
 
-Area of sphere:331.4
-Volume of sphere:523.5950000000001
-Area of Cone:175.5313362337335
-Volume of Cone:262.19
-Area of cylinder:-212.5
-Volume of cylinder:785.0
-Area of box:250.0
-Volume of box:250.0
+$ java Main9
+
+Area of sphere : 331.4
+Volume of sphere : 523.5950000000001
+Area of Cone : 175.5313362337335
+Volume of Cone : 262.19
+Area of cylinder : 212.5
+Volume of cylinder : 785.0
+Area of box : 250.0
+Volume of box : 250.0
 
 */
