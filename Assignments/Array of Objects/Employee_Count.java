f631a8c529ef4849449252a710a4426de393c379
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-02-23 14:06:47 +0530

    Example of counting the nmber of objects using static block

--- Assignments/Array of Objects/Employee_Count.java
+++ Assignments/Array of Objects/Employee_Count.java
@@ -1,8 +1,6 @@
 /*
 
-1. Define an Employee class (name, position, salary). Define a default and parameterized constructor. Override the toString method. Keep a count objects created. Create objects using parameterized constructor and display the object count after each object is created. (Use static member and method). Also display the contents of each object.
-
-2. Create a class MyDate with day, month and year as members. Write appropriate member functions. Create another class Employee_Count, which has id, name, date of birth, date of joining and salary as members (use MyDate for date fields). Write appropriate constructor for the Employee which assigns values to the members. Accept the details as command line arguments and create a Employee object using the arguments. Display the payslip details in a proper format.
+Create a class MyDate with day, month and year as members. Write appropriate member functions. Create another class Employee_Count, which has id, name, date of birth, date of joining and salary as members (use MyDate for date fields). Write appropriate constructor for the Employee which assigns values to the members. Accept the details as command line arguments and create a Employee object using the arguments and display the object count after each object is created. (Use static member and method). Also display the payslip details in a proper format.
 
 */
 
@@ -13,9 +11,9 @@ class MyDate
 	{}
 	MyDate(int dd,int mm,int yy)
 	{
-		this.dd=dd;
-		this.mm=mm;
-		this.yy=yy;
+		this.dd = dd;
+		this.mm = mm;
+		this.yy = yy;
 	}
 	
 }
@@ -27,29 +25,36 @@ class Employee_Count
 	double salary;
 	MyDate dob,doj;
 	static int objects;
-	static {
-		objects+=1;
+
+	static
+	{
+		objects += 1;
 	}
+
 	Employee_Count()
 	{}
+
 	Employee_Count(int id,String nm,String pos,double sal,int d1,int m1,int y1,int d2,int m2,int y2)
 	{
-		this.id=id;
-		name=nm;
-		position=pos;
-		salary=sal;
+		this.id = id;
+		name = nm;
+		position = pos;
+		salary = sal;
 		dob = new MyDate(d1,m1,y1);
 		doj = new MyDate(d2,m2,y2);
 	}
+
 	public String toString()
 	{
 		return("ID : "+id+"\nName : "+name+"\nPosition : "+position+"\nSalary : "+salary);
 	}
+
 	void display()
 	{
 		String str=String.format("Date of Birth : %d/%02d/%d\nDate of Joining : %d/%02d/%d",dob.dd,dob.mm,dob.yy,doj.dd,doj.mm,doj.yy);
 		System.out.println(str);
 	}
+
 	public static void main(String args[])
 	{
 		int id=Integer.parseInt(args[0]);
@@ -60,15 +65,16 @@ class Employee_Count
 		int d2 = Integer.parseInt(args[7]);
 		int m2 = Integer.parseInt(args[8]);
 		int y2 = Integer.parseInt(args[9]);
-		Employee_Count e=new Employee_COunt(id,args[1],args[2],salary,d1,m1,y1,d2,m2,y2);
+		Employee_Count e = new Employee_COunt(id,args[1],args[2],salary,d1,m1,y1,d2,m2,y2);
 		System.out.println(e+"\nNumber of objects created = "+objects);
 		e.display();
 	}
 }
 
-/*output
+/*
 
 $ javac Employee_Count.java
+
 $ java Employee_Count 1 Gaurav Manager 2000 12 05 1998 25 01 2019
 ID : 1
 Name : Gaurav
