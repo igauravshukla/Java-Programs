8e67fb3e37d00c24e10f1a1d635e4485d61b8243
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-02-24 13:35:37 +0530

    Example of simple Java program using methods

--- Assignments/Simple Java Program/MyNumber.java
+++ Assignments/Simple Java Program/MyNumber.java
@@ -9,40 +9,40 @@ class MyNumber
 	private int no;
 	MyNumber()
 	{
-		no=0;
+		no = 0;
 	}
 	MyNumber(int no)
 	{
-		this.no=no;
+		this.no = no;
      	}
 	void ispositive()
 	{
-		if(no>0)
+		if(no > 0)
 			System.out.print("Given Number is positive\n");
 	}
 	void isnegative()
 	{
-		if(no<0)
+		if(no < 0)
 			System.out.print("Given Number is negative\n");
 	}
 	void iseven()
 	{
-		if(no%2==0)
+		if(no%2 == 0)
 			System.out.print("Given Number is Even\n");
 	}
 	void isodd()
 	{
-		if(no%2!=0)
+		if(no%2 != 0)
 			System.out.print("Given Number is Odd\n");
 	}
 	void iszero()
 	{
-		if(no==0)
+		if(no == 0)
 			System.out.print("Given Number is zero\n");
 	}
 	public static void main(String args[])
 	{
-		int n=Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[0]);
 		MyNumber n1 = new MyNumber(n);
 		n1.ispositive();
 		n1.isnegative();
@@ -51,3 +51,33 @@ class MyNumber
 		n1.iszero();
 	}
 }
+
+/*
+
+$ javac MyNumber.java
+
+$ java MyNumber
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 0
+        at MyNumber.main(MyNumber.java:45)
+
+$ java MyNumber 2
+Given Number is positive
+Given Number is Even
+
+$ java MyNumber 1
+Given Number is positive
+Given Number is Odd
+
+$ java MyNumber 0
+Given Number is Even
+Given Number is zero
+
+$ java MyNumber -3
+Given Number is negative
+Given Number is Odd
+
+$ java MyNumber -4
+Given Number is negative
+Given Number is Even
+
+*/
