f64784ba6a5444437cd4d56b01ec7895b801e986
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-03-03 20:48:53 +0530

    Example of simple Java Program using methods

--- Assignments/Simple Java Program/MyNumber.java
+++ Assignments/Simple Java Program/MyNumber.java
@@ -1,8 +1,6 @@
 /*
 
-Define a class MyNumber having one private int data member. Write a default constructor to initialize it to 0 and another constructor to initialize it to a value (Use this). Write methods
-isNegative, isPositive, isZero, isOdd, isEven. Create an object in main. Use command line
-arguments to pass a value to the object.
+Define a class MyNumber having one private int data member. Write a default constructor to initialize it to 0 and another constructor to initialize it to a value (Use this). Write methods isNegative, isPositive, isZero, isOdd, isEven. Create an object in main. Use command line arguments to pass a value to the object.
 
 */
 
