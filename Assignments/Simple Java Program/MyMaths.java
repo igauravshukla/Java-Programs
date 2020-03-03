f364168f442313224df2d038752f90010b34569e
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-03-03 20:49:10 +0530

    Example of simple Java Program using methods.

---
+++ Assignments/Simple Java Program/MyMaths.java
@@ -0,0 +1,55 @@
+/*
+
+Define a class MyMaths having two private int data members. Write a default constructor to initialize it to 0 and another constructor to initialize it to a value (Use this). Write methods additionIS, subtractionIs , divisionIs, and multiplicationIs. Create an object in main. Use command line arguments to pass a value to the object.
+
+*/
+
+class MyMaths
+{
+	private int m,n;
+	MyMaths()
+	{
+	}
+	MyMaths(int x,int y)
+	{
+		this.m = x;
+		this.n = y;
+	}
+	void additionIs()
+	{
+		System.out.println("Addition = "+(m+n));
+	}
+	void subtractionIs()
+	{
+		System.out.println("Subtraction = "+(m-n));
+	}
+	void multiplicationIs()
+	{
+		System.out.println("Multiplication = "+(m*n));
+	}
+	void divisionIs()
+	{
+		System.out.println("Division = "+(m/n));
+	}
+	public static void main(String args[])
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		MyMaths m = new MyMaths(a,b);
+		m.additionIs();
+		m.subtractionIs();
+		m.multiplicationIs();
+		m.divisionIs();
+	}
+}
+
+/*output
+
+[cafy24@chb93 SET B]$ javac MyMaths.java
+[cafy24@chb93 SET B]$ java MyMaths 3 4
+Addition = 7
+Subtraction = -1
+Multiplication = 12
+Division = 0
+
+*/
