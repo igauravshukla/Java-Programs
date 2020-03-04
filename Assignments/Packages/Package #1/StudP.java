86ab4efb2ac929d7b19b6d7efc2db82d899fd9bd
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-03-04 23:42:00 +0530

    Example of creating a class in which different user defined classes are imported

---
+++ Assignments/Packages/Package #1/StudP.java
@@ -0,0 +1,77 @@
+import SY.SYMarks;
+import TY.TYMarks;
+import java.io.*;
+import java.util.*;
+
+class Stud
+{
+	int rno,tot;
+	String name,grade;
+	SYMarks sy;
+	TYMarks ty;
+	float avg;
+	Stud(int r,String n,SYMarks s,TYMarks t)
+	{
+		rno = r;
+		name = n;
+		sy = s;
+		ty = t;
+	}
+
+	void cal()
+	{
+		tot = sy.getct()+ty.gettotal();
+		avg = tot/5.0f;
+	}
+
+	void disp()
+	{
+		System.out.println(name+" "+rno+" "+tot+" "+grade);
+	}
+}
+
+class StudP
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter how many students");
+		int n = sc.nextInt();
+		Stud S[] = new Stud[n];
+		int ct,et,mt,th,pr,rno;
+		String name;
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Enter the roll no name of student: ");
+			rno = sc.nextInt();
+			name = sc.next();
+			System.out.println("Enter SY Marks(Electronics,Computer,Maths)");
+			et = sc.nextInt();
+			ct = sc.nextInt();
+			mt = sc.nextInt();
+			SYMarks sm = new SYMarks(et,ct,mt);
+			sm.disp1();
+			System.out.println("Enter TY Marks(Theory,Practical)");
+			th = sc.nextInt();
+			pr = sc.nextInt();
+			TYMarks tm = new TYMarks(th,pr);
+			tm.disp2();
+			S[i] = new Stud(rno,name,sm,tm);
+		}
+		for(int i=0;i<n;i++)
+		{
+			S[i].cal();
+			if(S[i].avg>=70)
+				S[i].grade = "A";
+			else if(S[i].avg>=60)
+				S[i].grade = "B";
+			else if(S[i].avg>=50)
+				S[i].grade = "C";
+			else if(S[i].avg>=40)
+				S[i].grade = "Pass";
+			else
+				S[i].grade = "Fail";
+			S[i].disp();
+		}
+	}
+}
