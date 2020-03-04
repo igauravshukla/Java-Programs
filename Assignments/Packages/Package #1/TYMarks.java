6465e3b0c5de38e1bd5493ea123bde096359979c
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-03-04 23:41:50 +0530

    Create TYMarks.java

---
+++ Assignments/Packages/Package #1/TYMarks.java
@@ -0,0 +1,21 @@
+package TY;
+public class TYMarks
+{
+	public int tm,pm;
+	public TYMarks(int x,int y)
+	{
+		tm = x;
+		pm = y;
+	}
+
+	public  void disp2()
+	{
+		System.out.println("Theory marks ="+tm);
+		System.out.println("Practical marks ="+pm);
+	}
+
+	public int gettotal()
+	{
+		return (tm+pm);
+	}
+}                                                                
