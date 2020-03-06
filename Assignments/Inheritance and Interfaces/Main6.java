8c75f4f8e21b620a4592551aec1f9893332c4726
Gaurav Shukla <gauravcsshukla999@gmail.com>
2020-03-06 17:23:16 +0530

    Example of object cloning

---
+++ Assignments/Inheritance and Interfaces/Main6.java
@@ -0,0 +1,70 @@
+import java.io.*;
+
+class Student
+{
+	int rno;
+	String name;
+	Student(int r,String nm)
+	{
+		rno=r;
+		name=nm;
+	}
+}
+
+class CR extends Student implements Cloneable
+{
+	String cname;
+	CR(int r,String nm,String c)
+	{
+		super(r,nm);
+		cname=c;
+	}
+	public CR clone() throws CloneNotSupportedException
+	{
+		CR C = (CR)super.clone();
+		return C;
+	}
+	public String toString()
+	{
+		return "Roll no = "+rno+" Name = "+name+" Class = "+cname;
+	}
+}
+
+class Main6
+{
+	public static void main(String args[]) throws IOException, CloneNotSupportedException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("\nEnter the number of clone objects to be made: ");
+		int n = Integer.parseInt(br.readLine());
+		System.out.print("\nEnter roll no of student:- ");
+		int rno = Integer.parseInt(br.readLine());
+		System.out.print("Enter name of student:- ");
+		String nm = br.readLine();
+		System.out.print("Enter class of student:- ");
+		String cls = br.readLine();
+		CR c= new CR(rno,nm,cls);
+		CR cl[] = new CR[n];
+		for(int i=0;i<n;i++)
+		{
+			cl[i] = c.clone();
+			System.out.println(cl[i]);
+		}
+	}
+}
+
+/*output
+
+$ javac Main6.java
+$ java Main6
+
+Enter the number of clone objects to be made: 3
+
+Enter roll no of student:- 1
+Enter name of student:- Gaurav
+Enter class of student:- MCA
+Roll no = 1 Name = Gaurav Class = MCA
+Roll no = 1 Name = Gaurav Class = MCA
+Roll no = 1 Name = Gaurav Class = MCA
+
+*/
